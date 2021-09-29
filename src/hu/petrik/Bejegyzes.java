9af6d0d7a85bb64215caa02f0a32b2ab934f9527
Schmidt Gergő <s.gergo.az@gmail.com>
2021-09-29 14:55:35 +0200

    getterek,setterek,konstruktor és funkciók létrehozása

--- src/hu/petrik/Bejegyzes.java
+++ src/hu/petrik/Bejegyzes.java
@@ -8,4 +8,40 @@ public class Bejegyzes {
     private int likeok;
     private LocalDateTime letrejott;
     private LocalDateTime szerkesztve;
+
+    public Bejegyzes(String szerzo, String tartalom) {
+        this.szerzo = szerzo;
+        this.tartalom = tartalom;
+        this.likeok=0;
+        this.letrejott=LocalDateTime.now();
+        this.szerkesztve=LocalDateTime.now();
+    }
+
+    public String getSzerzo() {
+        return this.szerzo;
+    }
+
+    public String getTartalom() {
+        return this.tartalom;
+    }
+
+    public int getLikeok() {
+        return this.likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return this.letrejott;
+    }
+
+    public LocalDateTime getSzerkesztve() {
+        return this.szerkesztve;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+        this.szerkesztve=LocalDateTime.now();
+    }
+    public void like(){
+        this.likeok+=1;
+    }
 }
